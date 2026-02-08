21c42615b82c598901106f52a59776589712c785
nachoyubero <202310138@alu.comillas.edu>
2026-02-08 17:07:55 +0100

    "Modificación de la declaración de la URL"

--- src/main/java/edu/comillas/icai/gitt/pat/spring/mvc/controlador/CarritoControlador.java
+++ src/main/java/edu/comillas/icai/gitt/pat/spring/mvc/controlador/CarritoControlador.java
@@ -9,37 +9,38 @@ import java.util.HashMap;
 import java.util.Map;
 
 @RestController
+@RequestMapping("/api/carritos")
 public class CarritoControlador {
     private final Map<Integer, Carrito> carritos = new HashMap<>();
 
     //De esta manera se gestiona la petición de los carritos
-    @GetMapping("/api/carrito")
+    @GetMapping
     public Collection<Carrito> getCarritos() {
         return carritos.values();
     }
 
 
     // Aquí se crea el carrito
-    @PostMapping("/api/carrito/")
+    @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public Carrito crearCarrito(@RequestBody Carrito carrito) {
         carritos.put(carrito.getIdCarrito(), carrito);
         return carrito;
     }
     //Hacemos un get para un solo carrito
-    @GetMapping("/api/carrito/{idCarrito}")
+    @GetMapping("/{idCarrito}")
     public Carrito getCarrito(@PathVariable int  idCarrito) {
         return carritos.get(idCarrito);
     }
 
-    @PutMapping("/api/carrito/{idCarrito}")
+    @PutMapping("/{idCarrito}")
     public Carrito modificarCarrito(@PathVariable int idCarrito, @RequestBody Carrito carrito) {
         carritos.put(idCarrito, carrito);
         return carrito;
     }
 
     //Aquí en este caso el Delete es muy parecido al GET
-    @DeleteMapping("/api/carrito/{idCarrito}")
+    @DeleteMapping("/{idCarrito}")
     public void borrarCarrito(@PathVariable int  idCarrito) {
         carritos.remove(idCarrito);
     }
